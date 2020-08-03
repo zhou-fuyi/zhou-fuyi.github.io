4d81a905eeef56c1d6b8529369a1e507eae2f401
zhoujian <thread_zhou@126.com>
2020-08-03 22:44:07 +0800

    1、更新构建方式

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:thread-zhou/thread-zhou.github.io.git
+git push -f git@github.com:thread-zhou/thread-zhou.github.io.git master:gh-pages
 
 cd -
