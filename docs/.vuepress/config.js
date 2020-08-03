db6d319d44983aa356c12db7f40dcd8dea1e7767
zhoujian <thread_zhou@126.com>
2020-08-03 23:40:50 +0800

    1、添加导航信息

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,11 @@
 module.exports = {
     // base: '/thread-zhou.github.io/',
     title: '边见众生，边见自己',
-    description: '我可以从天黑睡到天亮，再从天亮睡到天黑'
+    description: '我可以从天黑睡到天亮，再从天亮睡到天黑'，
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Socket', link: '/socket/' },
+        ]
+    }
 }
